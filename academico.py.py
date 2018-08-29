3131b8031cbecc080d66a8354bdff0883ed8a330
“daniel <daniel1997obando@gmail.com>
2018-08-28 21:17:42 -0400

    academic11

---
+++ academico.py.py
@@ -0,0 +1,18 @@
+sumaedades = 0
+promedio = 0
+edad1 = 15
+persona1 = ("juan carlos Arteaga")
+edad2 = 18
+persona2 = ("Angela maria lopez")
+edad3 = 22
+persona3 = ("roberto Andres jurado")
+edad4 = 19
+persona4 = ("monica maribel hurtado")
+print ("sistema de informacion academico - promedio de edades")
+sumaedades = edad1+edad2+edad3+edad4
+print ("El estudiante" ,persona1," tiene " ,edad1," años")
+print ("El estudiante" ,persona2," tiene " ,edad2," años")
+print ("El estudiante" ,persona3," tiene " ,edad3," años")
+print ("El estudiante" ,persona4," tiene " ,edad4," años")
+promedio = sumaedades/4
+print ("El promedio de edades es: ",promedio)
