2cb4b636166b36515bbb3e4cacd2f1d914eb6b75
“daniel <daniel1997obando@gmail.com>
2018-09-11 13:44:13 -0400

    tabla

---
+++ ciclo1.py
@@ -0,0 +1,14 @@
+#numeros con ciclo while
+num = 1
+while num <= 10:
+    print (num)
+    num = num+1
+
+#tabla de multiplicar
+    tabla = 5
+    tabla = int(input("digite la tabla de multiplicar: "))
+    num = 1
+    while num <= 10 :
+        print (tabla, "*", num, "=", tabla * num)
+        num = num + 1
+        
