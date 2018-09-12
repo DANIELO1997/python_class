2735347f53b2e76d5b1617ecbe161e3ea020f89b
“daniel <daniel1997obando@gmail.com>
2018-09-12 01:55:52 -0400

    10tablas

---
+++ ciclo3.py
@@ -0,0 +1,7 @@
+#numeros con ciclo while
+tabla = int(input("digite la tabla a multiplicar: "))
+rango = int(input("digite el rango a multiplicar: "))
+num=1 
+while num<=rango:
+    print tabla, "*",num,"=",tabla*num
+num=num+1
