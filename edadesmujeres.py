f6a78cc392476eb0960b5f4357605efac600d7d1
“daniel <daniel1997obando@gmail.com>
2018-09-12 02:02:38 -0400

    totaldeedades

---
+++ edadesmujeres.py
@@ -0,0 +1,9 @@
+i = 1
+pomedioedadesdemujeres = 0
+sumadeedades = 0
+edad = 0
+while i <=10 :
+    edad = int(input("ingrese edad: "))
+    sumaedades == sumaedades + edadp
+else "promedio = sumaedades / 10 :
+print("el promedio de edades de mujeres:", promedio)
